9d582d8ab40fcac19e9245702959834644bd19d8
Eryk Knesz <eryk.knesz@gmail.com>
2022-12-09 17:10:05 +0100

    change from BS4 to selenium due to dynamic loading

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 URL = "https://www.whoscored.com/Statistics"
+team_statistics_id = "top-team-stats-summary-grid"
+player_statistics_id = "top-player-stats-summary-grid"
 
-page = requests.get(URL).text
+chrome_driver_path = "/home/eryk/Development/108/chromedriver_linux64/chromedriver"
+service = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+driver.get(URL)
 
-with open("footballstats.html", "w") as file:
-    file.write(page)
+teams_table = driver.find_elements(By.ID, team_statistics_id)
+players_table = driver.find_elements(By.ID, player_statistics_id)
 
-with open("footballstats.html") as file:
-    soup = BeautifulSoup(file, "html.parser")
 
-titles = soup.find_all(name="h3", class_="title")
-
-with open("footballstats.csv", "a") as file:
-    for title in reversed(titles):
-        file.write(f"{title.string}\n")
 
 
